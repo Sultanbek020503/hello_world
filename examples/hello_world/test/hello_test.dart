3997fb2f0c920995a72d96467881ad97bb3aff27
Sultan020503 <2110193@newuzbekistanuniversity.uz>
2023-10-30 23:08:36 +0500

    1

--- examples/hello_world/test/hello_test.dart
+++ examples/hello_world/test/hello_test.dart
@@ -1,11 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hello_world/main.dart' as hello_world;
-
+import 'dart:ui';
 void main() {
   testWidgets('Hello world smoke test', (WidgetTester tester) async {
-    hello_world.main(); // builds the app and schedules a frame but doesn't trigger one
+    hello_world
+        .main(); // builds the app and schedules a frame but doesn't trigger one
     await tester.pump(); // triggers a frame
 
     expect(find.text('Hello, world!'), findsOneWidget);
   });
+
+  print('Hello world! Sultanbek');
 }
